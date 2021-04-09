7014050d41e042cda2ca33f2aa647cf406e2dc7c
Daniel Salazar <daniel.salazar@mail.utoronto.ca>
2021-04-08 23:18:52 -0400

    aa

--- wow.sh
+++ wow.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
 # This is the official Weeb OwOScript Wompiler :3 (wow)
+noWhite=""
 
-sourceCode=$1
-while getopts "w" opt; do
+while getopts ":w" opt; do
     case ${opt} in
-    w) noWhite="-w" ;;
+    w)
+        noWhite="-w"
+        ;;
+    \?) ;;
 
-    \?) wnoWhite="" ;;
     esac
 done
+shift $((OPTIND - 1))
+sourceCode=$1
 
 defaultVer=$(python --version 2>/dev/null)
 v3=" 3."
 if [[ "$defaultVer" == *"$v3"* ]]; then
-    python ./parser.py $sourceCode && (python ./toJS.py "$sourceCode.json" $noWhite) && rm "$sourceCode.json"
+    python ./parser.py $sourceCode && (python ./toJS.py $noWhite "$sourceCode.json") && rm "$sourceCode.json"
 else
     pythonVer=$(which python3 2>/dev/null)
     v3="python3"
     if [[ "$pythonVer" == *"$v3"* ]]; then
-        python3 ./parser.py $sourceCode && (python3 ./toJS.py "$sourceCode.json" $noWhite) && rm "$sourceCode.json"
+        python3 ./parser.py $sourceCode && (python3 ./toJS.py $noWhite "$sourceCode.json") && rm "$sourceCode.json"
     else
         echo "Requires python3"
     fi
 fi
+sleep 100
