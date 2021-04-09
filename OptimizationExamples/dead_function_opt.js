411702b87083275ac95a8bc38f0f64d1c6b5c272
Georges Hanna <georges.hanna@mail.utoronto.ca>
2021-04-09 17:32:17 -0400

    Optimized examples

---
+++ OptimizationExamples/dead_function_opt.js
@@ -0,0 +1,9 @@
+const range = (x) => {
+	let bruh = [];
+	for (let y = 0; y < x; y++) {
+		bruh = bruh.concat([y]);
+	};
+	return bruh;
+};
+let x = 10;
+console.log(x);
---
+++ OptimizationExamples/dead_variables_opt.js
@@ -0,0 +1,5 @@
+let x = -6;
+const nyan = true;
+let a = [1, 2, 3];
+const aa = [a, a, []];
+console.log(aa);
