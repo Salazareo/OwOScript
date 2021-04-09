8b7f80904153b644f9a202a31e9dff84ead8147e
Daniel Salazar <daniel.salazar@mail.utoronto.ca>
2021-04-08 22:05:16 -0400

    aaaaaaaa

--- wow.sh
+++ wow.sh
@@ -3,5 +3,16 @@
 # This is the official Weeb OwOScript Wompiler :3 (wow)
 
 sourceCode=$1
-(python ./parser.py $sourceCode || python3 ./parser.py $sourceCode) && (python ./toJS.py "$sourceCode.json" || python3 ./toJS.py "$sourceCode.json")
+
+(python3 --version 2>/dev/null) && (python3 ./parser.py $sourceCode && python3 ./toJS.py "$sourceCode.json")
+
+defaultVer=$(python --version)
+
+v3=" 3."
+
+if [[ "$defaultVer" == *"$v3"* ]]; then
+    python ./parser.py $sourceCode && python ./toJS.py "$sourceCode.json"
+else
+    echo "you need python3 for this"
+fi
 rm "$sourceCode.json"
