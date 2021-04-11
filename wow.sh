3350f332e4279b45ac925952ae7d765a2dc04ff7
Daniel Salazar <daniel.salazar@mail.utoronto.ca>
2021-04-10 22:17:30 -0400

    sssss

--- wow.sh
+++ wow.sh
@@ -18,12 +18,12 @@ sourceCode=$1
 defaultVer=$(python --version 2>/dev/null)
 v3=" 3."
 if [[ "$defaultVer" == *"$v3"* ]]; then
-    python ./parser.py $sourceCode && (python ./toJS.py $noWhite "$sourceCode.json") && rm "$sourceCode.json"
+    python ./OwOParser.py $sourceCode && (python ./toJS.py $noWhite "$sourceCode.json") && rm "$sourceCode.json"
 else
     pythonVer=$(which python3 2>/dev/null)
     v3="python3"
     if [[ "$pythonVer" == *"$v3"* ]]; then
-        python3 ./parser.py $sourceCode && (python3 ./toJS.py $noWhite "$sourceCode.json") && rm "$sourceCode.json"
+        python3 ./OwOParser.py $sourceCode && (python3 ./toJS.py $noWhite "$sourceCode.json") && rm "$sourceCode.json"
     else
         echo "Requires python3"
     fi
